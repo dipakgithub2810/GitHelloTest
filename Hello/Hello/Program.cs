66c0f8121f1f73d89159a8643dacb9465b5c631b
dipak-s <dipak-s@LP-5CD6518SPQ>
2019-01-16 11:36:51 +0530

    Commit

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(createmessage());
+        }
+
+        public static string createmessage()
+        {
+            return "Hello Word";
+        }
+    }
+}
